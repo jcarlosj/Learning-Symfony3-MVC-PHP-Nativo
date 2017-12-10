51806c907ab1adcd44c550c4508487bcfdfea4dc
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2017-12-10 12:10:58 -0500

    Refactoriza el Controlador

--- controllers/Person_Controller.php
+++ controllers/Person_Controller.php
@@ -1,9 +1,15 @@
 <?php
-  # Llamada al Modelo
-  require_once 'models/Person_Model.php';
-  $person = new Person_Model();
-  $data = $person -> get_people();
+  class Person_Controller {
 
-  # Llamada a la Vista
-  require_once 'views/people-view.phtml';
+    # Constructor
+    public function __construct() {
+      # Llamada al Modelo
+      require_once 'models/Person_Model.php';
+      $person = new Person_Model();
+      $data = $person -> get_people();
+
+      # Llamada a la Vista
+      require_once 'views/people-view.phtml';  
+    }
+  }
 ?>
--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
   require_once 'db/DataBase.php';
   require_once 'controllers/Person_Controller.php';
+  $person = new Person_Controller();
 ?>
